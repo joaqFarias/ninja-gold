ff8524bc6ae439f6633e27469d2d1573aff9fe83
Joaquin Farias <jfarias@lythium.cl>
2021-08-06 04:16:08 -0400

    se hace esqueleto de html con un poquitito de css, falta hacer la logica del back-end y luego ponerlo mas bonito

--- ninja_gold/casino/views.py
+++ ninja_gold/casino/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def root(request):
+    return redirect('/casino')
+
+def index(request):
+    request.session['oro_actual'] = 0
+    return render(request, 'index.html') 
+
+
+
+
+# del ejercicio anterior !!!!!
+def random_word(request):
+    if 'contador' not in request.session:
+        request.session['contador'] = 0
+        request.session.save()
+    else:
+        request.session['contador'] = int(request.session['contador']) + 1
+        request.session.save()
+    if int(request.session['contador'])  == 0:
+        palabra = ''
+    else:
+        palabra = get_random_string(length=32)
+    context = {
+        'palabra_random': palabra
+    }
+    return render(request, 'index.html', context)
+
+def reset(request):
+    del request.session['contador']
+    return redirect('/random_word')
